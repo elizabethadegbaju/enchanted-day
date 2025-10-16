ac112828bbecf8ff58b9bf7dc0e129fed53e7b82
Elizabeth Adegbaju <adeotunadegbaju@gmail.com>
2025-10-16 15:08:53 +0200

    update storage access for media files

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,4 +2,9 @@ import { defineStorage } from "@aws-amplify/backend";
 
 export const storage = defineStorage({
   name: "enchanted-day-media",
+  access: (allow) => ({
+    "media/{entity_id}/*" : [
+        allow.entity("identity").to(["read", "write", "delete"])
+    ]
+  })
 });
